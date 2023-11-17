f2dad34b2d8b3fd6dceb342f9d80b7b3f5b3c7b4
Mitpf <113504721+Mitpf@users.noreply.github.com>
2023-11-17 19:32:31 +0200

    add basic config file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('gsdfgsdf');
+const express = require('express');
+const config = require('./config.js');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.listen(config.PORT, () => console.log(`server is running on port ${config.PORT}`) );
