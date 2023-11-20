e4b2b08bc4d1725f0c2ed081bd3065b27dbb600d
Mitpf <113504721+Mitpf@users.noreply.github.com>
2023-11-20 21:59:41 +0200

    add express handlebars/config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const config = require('./config.js');
-
-
 const app = express();
+const config = require('./config');
+const setupViewEngine = require('./config/viewEngine.js');
+setupViewEngine(app);
+//require('./config/viewEngine.js')(app)//ugly code
+
 
 app.get('/', (req, res) => {
-    res.send('Home page');
+    res.render('home');
 });
 
-app.listen(config.PORT, () => console.log(`server is running on port ${config.PORT}`) );
+app.listen(config.PORT, () => console.log(`server is running on port ${config.PORT}`));
