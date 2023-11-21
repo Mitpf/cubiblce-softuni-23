d107394df3221ecefc33ad56469939f04ce9bea3
Mitpf <113504721+Mitpf@users.noreply.github.com>
2023-11-21 12:29:11 +0200

    setup static files/folder

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const setupViewEngine = require('./config/viewEngine.js');
 setupViewEngine(app);
 //require('./config/viewEngine.js')(app)//ugly code
 
+app.use(express.static('src/public'));
 
 app.get('/', (req, res) => {
     res.render('home');
