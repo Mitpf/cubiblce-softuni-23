5a694a2723cdb4a0267d8ed20310e03e91e7ed92
Mitpf <113504721+Mitpf@users.noreply.github.com>
2023-11-23 22:11:29 +0200

    add search

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,23 @@
 const db = require('../db.json');
 
 exports.getHomePage = (req, res) => {
-    res.render('index', { cubes: db.cubes });
+    const { search, from: difficultyFrom, to: difficultyTo } = req.query;
+    //const { search, from: difficultyFrom = 0, to: difficultyTo = Number.MAX_SAFE_INTEGER } = req.query;
+    let cubes = db.cubes;
+
+    if (search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (difficultyFrom) {
+        cubes = cubes.filter(cube => cube.difficultyLevel >= Number(difficultyFrom));
+    }
+
+    if (difficultyFrom) {
+        cubes = cubes.filter(cube => cube.difficultyLevel <= Number(difficultyTo));
+    }
+
+    res.render('index', { cubes, search, difficultyFrom, difficultyTo });
 }
 
 exports.getAboutPage = (req, res) => {
