b56ccc47668b60b1de96108694759b7d11d1daff
Mitpf <113504721+Mitpf@users.noreply.github.com>
2023-11-28 11:58:24 +0200

    details made work with mongoose

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,6 @@
 //const Cube = require('../models/Cube_old.js');
 const Cube = require('../models/Cube');
-const db = require('../db.json');
+
 
 //named export
 exports.getCreateCube = (req, res) => {
@@ -16,18 +16,12 @@ exports.postCreateCube = async (req, res) => {
 
 
 exports.getDetails = async (req, res) => {
-    let cubeId = Number(req.params.cubeId);
-
-    if (!cubeId) {
-        return res.redirect('/404');
-    }
-
-    let cube = await Cube.findById(cubeId).lean();
+    const cube = await Cube.findById(req.params.cubeId);
 
     if (!cube) {
         return res.redirect('/404');
     }
-    console.log('cuuuuuuube', cube)
+
     res.render('details', cube);
 };
 
